8d0154ea73c35b5fedf5558521bb65bddf1c3634
CasperOder <80528594+CasperOder@users.noreply.github.com>
2021-03-25 09:32:07 +0100

    Add files via upload

---
+++ RogueLike/RogueLike/Moveable_Object.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RogueLike
+{
+    class Moveable_Object : GameObject
+    {
+        protected int speed;
+        protected bool hasMoved;
+
+
+
+        public Moveable_Object(Texture2D tex, Rectangle hitbox) : base(tex, hitbox)
+        {
+
+        }
+
+        
+
+    }
+}
