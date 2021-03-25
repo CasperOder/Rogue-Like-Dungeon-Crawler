af71c75180ea5aa1b2b26d6074509580250102ab
CasperOder <casperder123@yahoo.se>
2021-03-25 09:51:30 +0100

    fixade gitignore

---
+++ RogueLike/Game1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RogueLike
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D kub;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            kub = Content.Load<Texture2D>(@"kub");
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(kub, Vector2.Zero, Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ RogueLike/Player.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLike
+{
+    class Player : Moveable_Object
+    {
+        Rectangle playerStartPos;
+
+        public Player(Texture2D tex, Rectangle hitbox) : base(tex, hitbox)
+        {
+
+        }
+
+        public void Movement()
+        {
+            if (!hasMoved)
+            {
+                playerStartPos = hitbox;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) //left
+            {
+                //Game1.GetPlayerCollision(new Rectangle(hitbox.X - speed, hitbox.Y, tileSize, tileSize));
+
+                //if (!isColliding)
+                {
+                    hitbox.X -= speed;
+                    hasMoved = true;
+
+
+
+                    //isColliding = false;
+                }
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right)) //right
+            {
+                //Game1.GetPlayerCollision(new Rectangle(hitbox.X + speed, hitbox.Y, tileSize, tileSize));
+
+                //if (!isColliding)
+                {
+                    hitbox.X += speed;
+                    hasMoved = true;
+
+
+
+                    //isColliding = false;
+                }
+            }
+
+            else if (Keyboard.GetState().IsKeyDown(Keys.Down)) //down
+            {
+                hasMoved = true;
+            }
+            //else
+            //{
+            //    isColliding = false;
+
+            //}
+        }
+
+    }
+}
