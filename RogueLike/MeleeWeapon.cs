0387689e3766677127b7b1a1c481a8483a4c51e5
IronGrahn <irongrahn@gmail.com>
2021-04-23 12:00:16 +0200

    Skapat Weapon, MeleeWeapon, RangeWeapon och Projectile klass
    Har skapat grundläggande klasser till combatsystemet. Saknas fortfarande viktiga aspekter.

---
+++ RogueLike/MeleeWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace RogueLike
+{
+    class MeleeWeapon:Weapon
+    {
+        Rectangle hitbox;
+
+        public MeleeWeapon(Rectangle hitbox, int damage, int range, float attackSpeed) : base(damage, range, attackSpeed)
+        {
+            this.hitbox = hitbox;
+        }
+        
+    
+
+        public void InflictDamage(Enemy e)
+        {
+            if(e.hitbox.Intersects(hitbox))
+            {
+
+            }
+        }
+
+
+
+    }
+}
---
+++ RogueLike/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RogueLike
+{
+    class Projectile:Moveable_Object
+    {
+        public Vector2 startPos;
+
+        public Projectile(Vector2 startPos ,int damage, SpriteSheet spriteSheet, double timeBetweenFrames) :base(spriteSheet, timeBetweenFrames)
+        {
+            this.startPos = startPos;
+            hitbox.Size = spriteSheet.frameSize;
+            middlepos = startPos;
+            hitbox.X = (int)middlepos.X - hitbox.Width / 2;
+            hitbox.Y = (int)middlepos.Y - hitbox.Height / 2;
+        }
+
+        public void Update()
+        {
+            middlepos += speed * direction;
+            hitbox.Location = new Point((int)middlepos.X, (int)middlepos.Y) - new Point(hitbox.Width / 2, hitbox.Height / 2);
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(spriteSheet.texture, hitbox, Color.White);
+        }
+
+    }
+}
