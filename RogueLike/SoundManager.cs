e9ddf162e6ecb27b15e057eaebdcd708dd756de4
CasperOder <casperder123@yahoo.se>
2021-05-05 18:55:56 +0200

    Test Song

---
+++ RogueLike/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLike
+{
+    public static class SoundManager
+    {
+        public static Song shopTheme;
+
+        //public static List<SoundEffect> playerSounds = new List<SoundEffect>();
+
+
+        public static void LoadAudio(ContentManager Content)
+        {
+            shopTheme = Content.Load<Song>(@"Pandora's Box");
+            //playerSounds.Add(Content.Load<SoundEffect>(@""));
+            //playerSounds.Add(Content.Load<SoundEffect>(@""));
+            //playerSounds.Add(Content.Load<SoundEffect>(@""));
+        }
+    }
+}
